6375f04dee1a38150586ccc1b1c6c9abd47fd2ba
OwenRamire <owenomar_ramirez@ucol.mx>
2020-11-08 15:57:18 -0600

    Use JSX double quoutes

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
 import './styles/myHeaderStyles.css';
 
 const MyHeader = () => {
-    return (
-        <header className='headerContainer'>
-            <img src={logo} className="image-logo" alt="logo" />
-            <h1 className='titleHeader'>React's to do list</h1>
-        </header>
-    );
-}
+  return (
+    <header className="headerContainer">
+      <img src={logo} className="image-logo" alt="logo" />
+      <h1 className="titleHeader">React's to do list</h1>
+    </header>
+  );
+};
 
 export default MyHeader;
