95e8a329ad595ef054869238bd84d0bd1c3934e4
OwenRamire <owenomar_ramirez@ucol.mx>
2020-11-09 00:33:38 -0600

    Import ActivityInput

--- src/components/AddToDo.jsx
+++ src/components/AddToDo.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {activities} from '../utils/activities';
 import ActivitiesUser from './ActivitiesUser';
+import ActivityInput from './ActivityInput';
 import './styles/addToDoStyles.css';
 
 class AddToDo extends Component {
@@ -23,8 +24,13 @@ class AddToDo extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    activities.push(this.state.activity);
-    alert('added');
+    if (this.state.activity === '') {
+      alert('Type an activity');
+    } else {
+      activities.push(this.state.activity);
+      alert('added');
+      this.setState({ activity: '' });
+    }
   };
 
   showActivities = () => {
@@ -42,15 +48,7 @@ class AddToDo extends Component {
       <div className="AddToDoContainer">
         <h1 className="title">What are we going to do?</h1>
         <form className="formActivities">
-          <label>
-            Activity
-            <input
-              placeholder="Your activity"
-              value={this.state.activity}
-              className="activityInput"
-              onChange={this.userType}
-            ></input>
-          </label>
+          <ActivityInput onChange={this.userType} value={this.state.activity} emptyInput={this.state.isInputEmpty}/>
           <button className="btnSubmit" onClick={this.onSubmit}>Add</button>
         </form>
         {this.showActivities()}
