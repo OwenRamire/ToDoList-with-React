7469adada1bc798cecf1412a6ccbc5cbb33d5749
OwenRamire <owenomar_ramirez@ucol.mx>
2020-11-17 00:06:33 -0600

    Use icons and give them styles

--- src/components/ActivitiesUser.jsx
+++ src/components/ActivitiesUser.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './styles/activitiesUserStyles.css';
 
 const ActivitiesUser = (props) => {
   const { activity } = props;
   return (
     <div className="activityContainer">
-      <p className="userActivity">{activity}</p>
+      <div className="userActivityContainer">
+        <p className="userActivity">{activity}</p>
+      </div>
+      <div className="iconsContainer">
+        <FontAwesomeIcon icon={faTimes} className="fontAwesomeIcons deleteIcon" />
+        <FontAwesomeIcon icon={faCheck} className="fontAwesomeIcons checkIcon" />
+      </div>
     </div>
   )
 };
